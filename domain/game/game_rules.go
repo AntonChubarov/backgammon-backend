c41730d29b7c0552a625125ce958cb3ff1e4c5af
Yurij Rubakha <yurij.osbb@gmail.com>
2021-08-19 13:44:03 +0300

    feat: + DiceInterpretationLongBackgammon

--- domain/game/game_rules.go
+++ domain/game/game_rules.go
@@ -7,3 +7,20 @@ type RulesKeeper interface {
 	PerformTurn(game *Game, stickColor *board.StickColor, turn *board.Turn) error
 	GetDiceInterpretation() func(d *board.DiceState) []int
 }
+
+func DiceInterpretationLongBackgammon(d *board.DiceState) []int {
+	var steps []int
+	if d.Dice1 == d.Dice2 {
+		steps = make([]int, 4, 4)
+		steps[0] = d.Dice1
+		steps[1] = d.Dice1
+		steps[2] = d.Dice1
+		steps[3] = d.Dice1
+		return steps
+
+	}
+	steps = make([]int, 2, 2)
+	steps[0] = d.Dice1
+	steps[1] = d.Dice2
+	return steps
+}
