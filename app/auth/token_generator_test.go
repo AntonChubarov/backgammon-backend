f76d25c250cc79b080764c518e2a64bdf68c7930
Anton Chubarov <achlp600@gmail.com>
2021-08-31 14:17:09 +0300

    test: token generator multi goroutine uniqueness

--- app/auth/token_generator_test.go
+++ app/auth/token_generator_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"log"
 	"regexp"
+	"sync"
 	"testing"
 )
 
@@ -24,7 +25,7 @@ func TestGenerateToken(t *testing.T) {
 	tokenRegex := fmt.Sprintf("^[a-zA-Z0-9]{%d}$", conf.Token.TokenLength)
 
 	for i := 0; i < n; i++ {
-		token:= generator.GenerateToken()
+		token := generator.GenerateToken()
 		isValid, err := regexp.MatchString(tokenRegex, string(token))
 		if err != nil {
 			log.Println(err)
@@ -40,12 +41,87 @@ func TestTokenGenerationUniqueness(t *testing.T) {
 	tokens := make([]authdomain.Token, 0, n)
 
 	for i := 0; i < n; i++ {
-		token:= generator.GenerateToken()
+		token := generator.GenerateToken()
 
 		isUnique = !utils.ContainsToken(&tokens, token)
 
 		assert.True(t, isUnique)
 		tokens = append(tokens, token)
+	}
+}
+
+func TestTokenGenerationUniqueness_Multi(t *testing.T) {
+	n := 10000
+	m := 6
+
+	var isUnique bool
+	tokens := make([]authdomain.Token, 0, n*m)
+
+	mu := sync.Mutex{}
+
+	wg := sync.WaitGroup{}
+	wg.Add(m)
+
+	for i := 0; i < m; i++ {
+		go func() {
+			for j := 0; j < 10000; j++ {
+				localToken := generator.GenerateToken()
+				mu.Lock()
+				tokens = append(tokens, localToken)
+				mu.Unlock()
+			}
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+
+	for i := 0; i < len(tokens); i++ {
+		token := tokens[i]
+		temp := tokens[i+1:]
+		isUnique = !utils.ContainsToken(&temp, token)
+		if !isUnique {
+			log.Println(token)
+		}
+		assert.True(t, isUnique)
+	}
+}
+
+func TestTokenGenerationUniqueness_MultiGenerator(t *testing.T) {
+	n := 10000
+	m := 6
+
+	var isUnique bool
+	tokens := make([]authdomain.Token, 0, n*m)
+
+	mu := sync.Mutex{}
+
+	wg := sync.WaitGroup{}
+	wg.Add(m)
+
+	for i := 0; i < m; i++ {
+		go func() {
+			localGenerator := NewTokenGeneratorFlex(&conf)
+			for j := 0; j < 10000; j++ {
+				localToken := localGenerator.GenerateToken()
+				mu.Lock()
+				tokens = append(tokens, localToken)
+				mu.Unlock()
+			}
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+
+	for i := 0; i < len(tokens); i++ {
+		token := tokens[i]
+		temp := tokens[i+1:]
+		isUnique = !utils.ContainsToken(&temp, token)
+		if !isUnique {
+			log.Println(token)
+		}
+		assert.True(t, isUnique)
 	}
 }
 
